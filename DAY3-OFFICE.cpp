7c237c24e3ab9bbebffe7670ecfa6b4909113e09
Mayank5o4 <mayankkumarsharma244@gmail.com>
2024-09-30 18:16:52 +0530

    Add files via upload

---
+++ DAY3-OFFICE.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        
+        int totalHours = 4 * X + Y;
+        cout << totalHours << endl;
+    }
+    
+    return 0;
+}
