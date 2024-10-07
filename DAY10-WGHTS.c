a8497066ab1653e079a722224da758597cac277b
Mayank5o4 <mayankkumarsharma244@gmail.com>
2024-10-07 22:20:09 +0530

    Update DAY10-WGHTS.c

--- DAY10-WGHTS.c
+++ DAY10-WGHTS.c
@@ -8,7 +8,7 @@ int main() {
         int W, X, Y, Z;
         scanf("%d %d %d %d", &W, &X, &Y, &Z); 
 
-        // Check all possible combinations
+        
         if (W == X || W == Y || W == Z || W == (X + Y) || W == (X + Z) || W == (Y + Z) || W == (X + Y + Z)) {
             printf("YES\n");
         } else {
