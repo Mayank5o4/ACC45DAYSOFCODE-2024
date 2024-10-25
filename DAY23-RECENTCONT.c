a922ad4ed2bee2baebd8d7425e74276bb7728562
Mayank5o4 <mayankkumarsharma244@gmail.com>
2024-10-25 20:06:46 +0530

    Add files via upload

---
+++ DAY23-RECENTCONT.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int T, N;
+    scanf("%d", &T);
+    
+    while (T--) {
+        scanf("%d", &N);
+        
+        int start38_count = 0;
+        int ltime108_count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            char contest_code[10];
+            scanf("%s", contest_code);
+            
+            if (strcmp(contest_code, "START38") == 0) {
+                start38_count++;
+            } else if (strcmp(contest_code, "LTIME108") == 0) {
+                ltime108_count++;
+            }
+        }
+        
+        printf("%d %d\n", start38_count, ltime108_count);
+    }
+    
+    return 0;
+}
+
+	
+
